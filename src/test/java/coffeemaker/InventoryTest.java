abebe620929628f56916b3f5b755721027375189
arw5395 <arw5395@rit.edu>
2024-09-12 10:50:04 -0400

    31 test cases

--- src/test/java/coffeemaker/InventoryTest.java
+++ src/test/java/coffeemaker/InventoryTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import coffeemaker.domain.Inventory;
@@ -39,39 +38,72 @@ public class InventoryTest {
     }
 
     /**
-     * Test that given a negative value for coffee, it should not update the amount
-     * of coffee in the inventory
+     * Set ingredient to Zero
+     */
+    @Test
+    public void testSetCoffee_Zero() {
+        CuT.setCoffee(0);
+        assertEquals(0, CuT.getCoffee());
+    }
+    @Test
+    public void testSetMilk_Zero() {
+        CuT.setMilk(0);
+        assertEquals(0, CuT.getMilk());
+    }
+    @Test
+    public void testSetSugar_Zero() {
+        CuT.setSugar(0);
+        assertEquals(0, CuT.getSugar());
+    }
+    @Test
+    public void testSetChocolate_Zero() {
+        CuT.setChocolate(0);
+        assertEquals(0, CuT.getChocolate());
+    }
+
+    /**
+     * Set ingredient to Integer.MAX_VALUE
+     */
+    @Test
+    public void testSetCoffee_Max() {
+        CuT.setCoffee(Integer.MAX_VALUE);
+        assertEquals(Integer.MAX_VALUE, CuT.getCoffee());
+    }
+    @Test
+    public void testSetMilk_Max() {
+        CuT.setMilk(Integer.MAX_VALUE);
+        assertEquals(Integer.MAX_VALUE, CuT.getMilk());
+    }
+    @Test
+    public void testSetSugar_Max() {
+        CuT.setSugar(Integer.MAX_VALUE);
+        assertEquals(Integer.MAX_VALUE, CuT.getSugar());
+    }
+    @Test
+    public void testSetChocolate_Max() {
+        CuT.setChocolate(Integer.MAX_VALUE);
+        assertEquals(Integer.MAX_VALUE, CuT.getChocolate());
+    }
+
+    /**
+     * Test that given a negative value for a ingredient, it should not update the
+     * amount in the inventory
      */
     @Test
     public void testSetCoffee_Negative() {
         CuT.setCoffee(-1);
         assertEquals(15, CuT.getCoffee());
     }
-
-    /**
-     * Test that given a negative value for milk, it should not update the amount of
-     * milk in the inventory
-     */
     @Test
     public void testSetMilk_Negative() {
         CuT.setMilk(-1);
         assertEquals(15, CuT.getMilk());
     }
-
-    /**
-     * Test that given a negative value for sugar, it should not update the amount
-     * of sugar in the inventory
-     */
     @Test
     public void testSetSugar_Negative() {
         CuT.setSugar(-1);
         assertEquals(15, CuT.getSugar());
     }
-
-    /**
-     * Test that given a negative value for chocolate, it should not update the
-     * amount of chocolate in the inventory
-     */
     @Test
     public void testSetChocolate_Negative() {
         CuT.setChocolate(-1);
@@ -79,7 +111,7 @@ public class InventoryTest {
     }
 
     /**
-     * Test that given a positive value for coffee, the amount of coffee in the
+     * Test that given a positive value for a ingredient, the amount of that ingredient in the
      * inventory is updated
      */
     @Test
@@ -87,91 +119,16 @@ public class InventoryTest {
         CuT.addCoffee("5");
         assertEquals(20, CuT.getCoffee());
     }
-
-    /**
-     * Test that given a negative value for coffee, an
-     * InventoryException is thrown
-     */
-    @Test
-    public void testAddCoffee_Negative() {
-        Exception e = assertThrows(InventoryException.class, () -> CuT.addCoffee("-1"));
-        assertTrue("Units of coffee must be a positive integer".equals(e.getMessage()));
-    }
-
-    /**
-     * Test that given a non-integer value for coffee, an
-     * InventoryException is thrown
-     */
-    @Test
-    public void testAddCoffee_NonInteger() {
-        Exception e = assertThrows(InventoryException.class, () -> CuT.addCoffee("a"));
-        assertTrue("Units of coffee must be a positive integer".equals(e.getMessage()));
-    }
-
-    /**
-     * Test that given a positive value for milk, the amount of milk in the
-     * inventory is updated
-     */
     @Test
     public void testAddMilk() {
         CuT.addMilk("5");
         assertEquals(20, CuT.getMilk());
     }
-
-    /**
-     * Test that given a negative value for milk, an
-     * InventoryException is thrown
-     */
-    @Test
-    public void testAddMilk_Negative() {
-        Exception e = assertThrows(InventoryException.class, () -> CuT.addMilk("-1"));
-        assertTrue("Units of milk must be a positive integer".equals(e.getMessage()));
-    }
-
-    /**
-     * Test that given a non-integer value for milk, an
-     * InventoryException is thrown
-     */
-    @Test
-    public void testAddMilk_NonInteger() {
-        Exception e = assertThrows(InventoryException.class, () -> CuT.addMilk("a"));
-        assertTrue("Units of milk must be a positive integer".equals(e.getMessage()));
-    }
-
-    /**
-     * Test that given a positive value for sugar, the amount of sugar in the
-     * inventory is updated
-     */
     @Test
     public void testAddSugar() {
         CuT.addSugar("5");
         assertEquals(20, CuT.getSugar());
     }
-
-    /**
-     * Test that given a negative value or non-integer for sugar, an
-     * InventoryException is thrown
-     */
-    @Test
-    public void testAddSugar_Negative() {
-        Exception e = assertThrows(InventoryException.class, () -> CuT.addSugar("-1"));
-        assertTrue("Units of sugar must be a positive integer".equals(e.getMessage()));
-    }
-
-    /**
-     * Test that given a non-integer value for sugar, an
-     * InventoryException is thrown
-     */
-    @Test
-    public void testAddSugar_NonInteger() {
-        Exception e = assertThrows(InventoryException.class, () -> CuT.addSugar("a"));
-        assertTrue("Units of sugar must be a positive integer".equals(e.getMessage()));
-    }
-
-    /**
-     * Test that given a positive value for chocolate, the amount of chocolate in
-     * the inventory is updated
-     */
     @Test
     public void testAddChocolate() {
         CuT.addChocolate("5");
@@ -179,9 +136,24 @@ public class InventoryTest {
     }
 
     /**
-     * Test that given a negative value or non-integer for chocolate, an
+     * Test that given a negative value for a ingredient, an
      * InventoryException is thrown
      */
+    @Test
+    public void testAddCoffee_Negative() {
+        Exception e = assertThrows(InventoryException.class, () -> CuT.addCoffee("-1"));
+        assertTrue("Units of coffee must be a positive integer".equals(e.getMessage()));
+    }
+    @Test
+    public void testAddMilk_Negative() {
+        Exception e = assertThrows(InventoryException.class, () -> CuT.addMilk("-1"));
+        assertTrue("Units of milk must be a positive integer".equals(e.getMessage()));
+    }
+    @Test
+    public void testAddSugar_Negative() {
+        Exception e = assertThrows(InventoryException.class, () -> CuT.addSugar("-1"));
+        assertTrue("Units of sugar must be a positive integer".equals(e.getMessage()));
+    }
     @Test
     public void testAddChocolate_Negative() {
         Exception e = assertThrows(InventoryException.class, () -> CuT.addChocolate("-1"));
@@ -189,9 +161,24 @@ public class InventoryTest {
     }
 
     /**
-     * Test that given a non-integer value for chocolate, an
+     * Test that given a non-integer value for a ingredient, an
      * InventoryException is thrown
      */
+    @Test
+    public void testAddCoffee_NonInteger() {
+        Exception e = assertThrows(InventoryException.class, () -> CuT.addCoffee("a"));
+        assertTrue("Units of coffee must be a positive integer".equals(e.getMessage()));
+    }
+    @Test
+    public void testAddMilk_NonInteger() {
+        Exception e = assertThrows(InventoryException.class, () -> CuT.addMilk("a"));
+        assertTrue("Units of milk must be a positive integer".equals(e.getMessage()));
+    }
+    @Test
+    public void testAddSugar_NonInteger() {
+        Exception e = assertThrows(InventoryException.class, () -> CuT.addSugar("a"));
+        assertTrue("Units of sugar must be a positive integer".equals(e.getMessage()));
+    }
     @Test
     public void testAddChocolate_NonInteger() {
         Exception e = assertThrows(InventoryException.class, () -> CuT.addChocolate("a"));
@@ -203,7 +190,7 @@ public class InventoryTest {
      * Test indirectly using useIngredients.
      */
     @Test
-    public void testEnoughIngredientsSufficient() {
+    public void testEnoughIngredients_Sufficient() {
         Recipe r = new Recipe();
         r.setAmtCoffee("5");
         r.setAmtMilk("5");
@@ -221,7 +208,7 @@ public class InventoryTest {
     }
 
     /**
-     * Test that enoughIngredients fails when insufficient amount of coffee
+     * Test that enoughIngredients fails when insufficient amount of an ingredient
      * Test indirectly using useIngredients.
      */
     @Test
@@ -234,19 +221,12 @@ public class InventoryTest {
 
         assertFalse(CuT.useIngredients(r),
                 "Should not be able to use ingredients due to insufficient amount of coffee");
-
-        // Verify inventory remains unchanged
         assertAll("Inventory should not change if ingredients are insufficient",
                 () -> assertEquals(15, CuT.getCoffee()),
                 () -> assertEquals(15, CuT.getMilk()),
                 () -> assertEquals(15, CuT.getSugar()),
                 () -> assertEquals(15, CuT.getChocolate()));
     }
-
-    /**
-     * Test that enoughIngredients fails when insufficient amount of milk
-     * Test indirectly using useIngredients.
-     */
     @Test
     public void testEnoughIngredients_InsufficientMilk() {
         Recipe r = new Recipe();
@@ -256,19 +236,12 @@ public class InventoryTest {
         r.setAmtChocolate("5");
 
         assertFalse(CuT.useIngredients(r), "Should not be able to use ingredients due to insufficient amount of milk");
-
-        // Verify inventory remains unchanged
         assertAll("Inventory should not change if ingredients are insufficient",
                 () -> assertEquals(15, CuT.getCoffee()),
                 () -> assertEquals(15, CuT.getMilk()),
                 () -> assertEquals(15, CuT.getSugar()),
                 () -> assertEquals(15, CuT.getChocolate()));
     }
-
-    /**
-     * Test that enoughIngredients fails when insufficient amount of sugar
-     * Test indirectly using useIngredients.
-     */
     @Test
     public void testEnoughIngredients_InsufficientSugar() {
         Recipe r = new Recipe();
@@ -278,19 +251,12 @@ public class InventoryTest {
         r.setAmtChocolate("5");
 
         assertFalse(CuT.useIngredients(r), "Should not be able to use ingredients due to insufficient amount of sugar");
-
-        // Verify inventory remains unchanged
         assertAll("Inventory should not change if ingredients are insufficient",
                 () -> assertEquals(15, CuT.getCoffee()),
                 () -> assertEquals(15, CuT.getMilk()),
                 () -> assertEquals(15, CuT.getSugar()),
                 () -> assertEquals(15, CuT.getChocolate()));
     }
-
-    /**
-     * Test that enoughIngredients fails when insufficient amount of chocolate
-     * Test indirectly using useIngredients.
-     */
     @Test
     public void testEnoughIngredients_InsufficientChocolate() {
         Recipe r = new Recipe();
@@ -301,19 +267,12 @@ public class InventoryTest {
 
         assertFalse(CuT.useIngredients(r),
                 "Should not be able to use ingredients due to insufficient amount of chocolate");
-
-        // Verify inventory remains unchanged
         assertAll("Inventory should not change if ingredients are insufficient",
                 () -> assertEquals(15, CuT.getCoffee()),
                 () -> assertEquals(15, CuT.getMilk()),
                 () -> assertEquals(15, CuT.getSugar()),
                 () -> assertEquals(15, CuT.getChocolate()));
     }
-
-    // @Test
-    // public void testToString() {
-    // ToStringVerifier.forClass(Inventory.class).verify();
-    // }
 
     @Test
     public void testToString() {
